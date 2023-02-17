7e8be2b82255ea03942c278ebe13d56930f783e5
Zuhaib Hashim <123729940+zuhaibhashim313@users.noreply.github.com>
2023-02-17 22:47:43 +0500

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -16,7 +16,7 @@ const guestbook = {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/save-guestbook-entry-sequence`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
